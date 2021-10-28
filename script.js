184cbe2e8b00ef998b38fd1fc62b43d667e39c42
ilya <slom20252025@gmail.com>
2021-10-28 15:08:10 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+// const result = confirm("Are u here?");
+// // console.log(result);
+
+// // let ans = prompt("сколько тебе лет?", "20");
+// // console.log(ans + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("как тебя зовут?", "");
+// answers[1] = prompt("твоя фамилия? ", "");
+// answers[2] = prompt("сколько тебе лет? ", "");
+
+// console.log(typeof(answers));
+
+// const user = "ilya";
+
+// alert(`привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+const isCheked = true,
+      isClose = true;
+
+console.log(isCheked && isClose);
