46cdbb7f7d9669c40a05296d78b6783a4e24ff34
ilya <slom20252025@gmail.com>
2021-11-03 19:59:34 +0300

    третий прак

--- 1script.js
+++ 1script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfilms = +prompt('Сколько фильмов вы уже посмотрели?');
+let numberOfilms
+
+function start() {
+    numberOfilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfilms == '' || numberOfilms == null || isNaN(numberOfilms)) {
+        numberOfilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfilms,
@@ -10,12 +20,60 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Последний просмотренный фильм?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Последний просмотренный фильм?'),
-      d = prompt('На сколько оцените его?');
+function remembrMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Последний просмотренный фильм?');
+        const b = prompt('На сколько оцените его?');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else {
+            console.log('error');
+            i--;
+        }
+    
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+remembrMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('мало');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('cредне');
+    } else if (personalMovieDB.count >=30) {
+        console.log('много');
+    } else {
+        console.log("произошла ошибка");
+    }
+    
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+        console.log("выводит")
+    } else if (personalMovieDB.privat == true) {
+        console.log('не выводит')
+    }
+    
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        const a = prompt(`Ваш любимый  жанр под номером ${i}`);
+
+        personalMovieDB.genres[i - 1] = a;
+    } 
+}
+
+writeYourGenres();
+
+// console.log(personalMovieDB);
+
--- script.js
+++ script.js
@@ -27,3 +27,108 @@
 //       isClose = true;
 
 // console.log(isCheked && isClose);
+
+
+// const num = 50;
+
+// switch (num) {
+//     case 51:
+//             console.log('верно');
+//             break;
+//     case 49:
+//         console.log('неверно');
+//         break;
+//     case 100:
+//         console.log('неверно');
+//         break;
+//     default:
+//         console.log('не в этот раз');
+//         break;
+// }
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let num 
+
+function showFirstMessage(text) {
+    console.log(text);
+    num = 20
+}
+
+showFirstMessage('Helloy world');
+console.log(num);
+// //
+// function calc(a, b) {
+//     return (a + b);
+// }                                     возвращение значения наружу 
+
+// console.log(calc(10, 6));
+//
+function ret() {
+    let num = 50;
+
+    return num
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+//
+const logger = function() {
+    console.log("hello")
+};
+
+logger();
+//
+const calc = (a, b) => {
+    console.log("1");
+};
+//
+                                //Методы и Сво-ва строк и чисел
+const str = "teXt",
+      arr = [1, 2, 3];
+
+
+console.log(str.toLowerCase());
+console.log(str);
+
+//
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("f"));
+//
+
+const log = "меня зовут илья";
+  console.log(log.slice(11, 14));    //аргументы start end
+
+console.log(log.substring(11, 14));
+
+console.log(log.substr(11, 3));  //start и длинна вырезки
+//
+
+const num = 12.2;
+console.log(Math.round(num));  //округление
+//
+const test = "12.2px";
+
+//console.log(parseInt(test));  // превращает в числовой тип данных
+console.log(parseFloat(test));
+
